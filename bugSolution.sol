f09b2c19a7db6b84145a40e5dc8e7ed1d11e1ee0
BugHunter-X <askzenai@gmail.com>
2025-01-23 22:10:43 +0100

    Add bugSolution.sol file

---
+++ bugSolution.sol
@@ -0,0 +1,25 @@
+function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        uint256 senderBalance = balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            balances[sender] = senderBalance - amount;
+        }
+        balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+    }
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
